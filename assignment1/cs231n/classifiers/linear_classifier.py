9f37b272dfcb78fcea0ee58c2e6f5eee660125ff
kai-xie <qikai_xie@163.com>
2017-09-12 22:55:38 +0800

    finish svm

--- assignment1/cs231n/classifiers/linear_classifier.py
+++ assignment1/cs231n/classifiers/linear_classifier.py
@@ -79,7 +79,7 @@ class LinearClassifier(object):
       #                       END OF YOUR CODE                                #
       #########################################################################
 
-      if verbose and it % 100 == 0:
+      if verbose and it % 200 == 0:
         print('iteration %d / %d: loss %f' % (it, num_iters, loss))
 
     return loss_history
